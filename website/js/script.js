3cc4ac12eeeef7ab522e2905f98099f3eee9f729
Arbaznazir <arbaznazir4@gmail.com>
2024-04-30 11:14:40 +0530

    Changed the style of whole website

--- website/js/script.js
+++ website/js/script.js
@@ -1,72 +1,35 @@
 
-const images = ["res/img/image1.jpg", "res/img/Image2.jpg", "res/img/Image3.jpg", "res/img/Image4.jpg"];
+var slideIndex = 0;
+showSlides();
 
-let currentImageIndex = 0;
-
-// Function to change the displayed image
-function changeImage(direction) 
-{
-	if (direction) 
-	{
-    		currentImageIndex += (direction === 'prev') ? -1 : 1;
-  	} else {
-    		currentImageIndex++; // Automatic change (no direction specified)
-  	}
-  
-  // Handle overflow and underflow
-  	if (currentImageIndex < 0) {
-    		currentImageIndex = images.length - 1;
-  	} else if (currentImageIndex >= images.length) {
-    		currentImageIndex = 0;
-  	}
-
-	document.getElementById("current-image").src = images[currentImageIndex];
+function showSlides() {
+  var slides = document.getElementsByClassName("mySlides");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
-// Start the automatic image change with a delay of 5 seconds
-window.onload = function() {
-  		setInterval(changeImage, 5000); // Change image every 5 seconds
-	};
+var mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-function changeColor(color)
-{
-		var clr1="#0A100D", clr2="#B9BAA3",clr3="#D6D5C9",clr4="#A22C29",clr5="#5B6C5D";
-
-	if(color=='c1')
-	{
-		document.getElementById('menubar').style.background=clr1;
-		document.getElementById('text-on-image').style.background=clr1;
-		document.getElementById('footer').style.background=clr1;
-	}
-	else if(color=='c2')
-	{
-		document.getElementById('menubar').style.background=clr2;
-		document.getElementById('text-on-image').style.background=clr2;
-		document.getElementById('footer').style.background=clr2;
-	}
-	else if(color=='c3')
-	{
-		document.getElementById('menubar').style.background=clr3;
-		document.getElementById('text-on-image').style.background=clr3;
-		document.getElementById('footer').style.background=clr3;
-	}
-	else if(color=='c4')
-	{
-		document.getElementById('menubar').style.background=clr4;
-		document.getElementById('text-on-image').style.background=clr4;
-		document.getElementById('footer').style.background=clr4;
-	}
-	else if(color=='c5')
-	{
-		document.getElementById('menubar').style.background=clr5;
-		document.getElementById('text-on-image').style.background=clr5;
-		document.getElementById('footer').style.background=clr5;
-	}
-	else
-	{
-		alert('Not a valid color');
-	}
-
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
